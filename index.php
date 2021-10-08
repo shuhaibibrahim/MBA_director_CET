4f0140c09ee8bf0d65a588cca319f2e30dcb9fe7
shuhaibibrahim <shuhaib.ibrahim2000@gmail.com>
2021-10-09 00:15:08 +0530

    first form

---
+++ index.php
@@ -0,0 +1,43 @@
+<html>
+<body>
+<?php
+
+?>
+<form>
+<div>   
+<div>Personal Details</div>
+<label for="name">Full name : </label><input id="name" type="text" name="name"><br/>
+<label for="dob">Date of Birth : </label><input id="dob" type="date" name="name"><br/>
+<label for="pob">Place of Birth (City, Country) : </label><input id="pob" type="text" name="name"><br/>
+<label for="gender">Gender : </label><input id="gender" type="text" name="name"><br/>
+<label for="nationality">Nationality : </label><input id="nationality" type="text" name="name"><br/>
+<label for="father">Father : </label><input id="father" type="text" name="name"><br/>
+<label for="marital">Marital Status : </label><input id="marital" type="text" name="name"><br/>
+</div>
+<br/>
+
+<div>
+<div>Current Contact Information</div>
+<label for="mobile">Mobile Number : </label><input id="mobile" type="text" name="mobile"><br/>
+<label for="phone">Phone Number : </label><input id="phone" type="text" name="phone"><br/>
+<label for="pemail">Primary Email Address : </label><input id="pemail" type="email" name="pemail"><br/>
+<label for="semail">Secondary Email Address : </label><input id="semail" type="email" name="semail"><br/>
+<label for="address">Current Address : </label><input id="address" type="text" name="address"><br/>
+<label for="father">Father : </label><input id="father" type="text" name="father"><br/>
+<label for="marital">Marital Status : </label><input id="marital" type="text" name="marital"><br/>
+</div>
+<br/>
+
+<div>
+<div>Teaching/Research Interests</div>
+<label for="rspec">Your Research Specialization : </label> <input id="rspec" type="text" name="rspec"><br/>
+<label for="stmt">Statement of your "Teaching/Research/ Administration agenda" for the next 3 yrs	:(2-3 sentences)</label><br/>
+<textarea id="stmt" name="stmt"rows="5" cols="33"></textarea><br/>
+<label for="courses">Name core of courses you like to teach at MBA level (You may refer to KTU Curricula)</label><input id="courses" type="text" name="courses"><br/>
+<label for="elcourses">Name any elective course you like to offer.	: (maximum 3 courses)</label><input id="text" type="elcourses" name="elcourses"><br/>
+</div>
+
+<input type="submit"/>
+</form>
+</body>
+</html>
