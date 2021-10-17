7170924738c40a33424061fafb2cadb9e83c69bd
shuhaibibrahim <shuhaib.ibrahim2000@gmail.com>
2021-10-17 20:46:58 +0530

    final

--- frontend/src/AlreadySubmitted.js
+++ frontend/src/AlreadySubmitted.js
@@ -3,7 +3,7 @@ import React from "react";
 function AlreadySubmitted({ logout }) {
     return (
         <div className="flex-75 flex justify-center items-center">
-            <h2 className="text-3xl font-light text-gray-500 mb-2">You have submitted!</h2>
+            <h2 className="text-3xl font-light text-gray-500 mb-2">Application submitted!</h2>
         </div>
     );
 }
