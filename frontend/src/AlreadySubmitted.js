99e32f8c31613d6977d8602454b032120abbdff8
N Athul Kumar <athulnvinod@gmail.com>
2021-10-17 10:35:42 +0530

    changes

--- frontend/src/AlreadySubmitted.js
+++ frontend/src/AlreadySubmitted.js
@@ -4,7 +4,7 @@ import TitleSVG from "./TitleSVG";
 function AlreadySubmitted({logout}) {
     return (
         <div className="flex-75 flex justify-center items-center">
-            <h2 className="text-3xl font-light text-gray-500 mb-2">You have already submitted!</h2>
+            <h2 className="text-3xl font-light text-gray-500 mb-2">You have submitted!</h2>
         </div>
     )
 }
